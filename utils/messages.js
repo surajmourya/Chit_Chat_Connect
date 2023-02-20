2c29df7a3730b2d88a171dfa3836598db2894b8f
Suraj Mourya <surajmourya0207@gmail.com>
2023-02-21 00:24:43 +0530

    commit

---
+++ utils/messages.js
@@ -0,0 +1,24 @@
+// const moment = require('moment');
+
+// function formatMessage(username, text){
+//     return {
+//         username,
+//         text,
+//         time: moment().format('h:mm a')
+//     }
+// }
+
+// module.exports = formatMessage
+
+
+const moment = require('moment');
+
+function formatMessage(username, text) {
+  return {
+    username,
+    text,
+    time: moment().format('h:mm a')
+  };
+}
+
+module.exports = formatMessage;
